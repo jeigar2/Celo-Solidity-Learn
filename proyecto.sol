ef06f7f07db1625a821b4458c99099712092c73a
Jesus García <jeigar2@gmail.com>
2022-03-26 01:38:13 +0100

    se va completando parte del proyecto

--- proyecto.sol
+++ proyecto.sol
@@ -14,17 +14,34 @@ contract Proyecto {
     unit public montoPago;
 
     constructor(address _comprador, address _vendedor, uint _monto, address _arbitro){
+        comprador= _comprador;
+        vendedor= _vendedor;
+        montoPago= _monto;
+        arbirto = _arbitro;
+        depositoListo=false;
+        pagoListo=false;
     }
 
     //deposita el comprador
     function despositarPago() public {}
 
     function compradorConfirmaOK() public {
+        require(msg.sender==comprador,"No es el comprador");
+        compradorOK=true;
     }
 
     //retira el vendedor
-    function retirarPago() public {}
+    function retirarPago() public {
+        require(compradorOK,"El comprador no ha dado el  ok");
+        payable(vendedor).transfer(montoPago);
+        pagoListo=true;
+
+    }
 
     // si el vendedor no entrega el producto, interviene el _arbitro
-    function cancelarPorArbitro() public {}
+    function cancelarPorArbitro() public {
+        require(msg-sender==arbitro,"Solo lo puede hacer el arbirto");
+        payable(vendedor).transfer(montoPago);
+        pagoListo=true;
+    }
 } 
